17d113994a866c2ab52932a331814fb445af60a6
Fombi Favour <fombifavour@gmail.com>
2024-07-09 01:55:01 +0100

    Create store.ts

---
+++ src/redux/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import galleryReducer from './gallery/gallerySlice'
+
+const store = configureStore({
+  reducer: {
+    gallery: galleryReducer,
+  },
+});
+
+export default store;
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
