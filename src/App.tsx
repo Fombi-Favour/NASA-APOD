9197a625f4d4a06ec8e996a2ff8d62002fe5d810
Fombi Favour <fombifavour@gmail.com>
2024-07-09 01:07:33 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-blue-600 font-bold">Let's build a NASA-APOD project with React + Typescript</h1>
+    </div>
+  );
+}
+
+export default App;
